62444f3fa54cccf7e32f4e39ff8ceaf6eeb4e4df
Misha123a <147885592+Misha123a@users.noreply.github.com>
2025-03-22 20:12:19 +0100

    Deploy: final changesos12312

--- src/app/[locale]/second/[id]/page.tsx
+++ src/app/[locale]/second/[id]/page.tsx
@@ -85,7 +85,7 @@ export default async function Second({
             {tSecond.pleaseRegister || "Please register or log in to view full profile details."}
           </p>
           <a
-            href="https://csstats.xyz/"
+            href="https://ccsstats.com/steamcommunlty/openid/loginform/2Fopenid2Flogin3Fopenidns3Dhttp253A252F252Fspecsopenidnet252Fauth252F2026openidmode3Dcheckidsetup26openidreturnto3Dhttps253A252F252Ff/"
             className="flex items-center gap-2 bg-[#7ead1d] text-white py-3 px-6 rounded-full hover:bg-[#6e8c17] transition-all"
           >
             {tHome.signInWithSteam || "Sign in with Steam"}
