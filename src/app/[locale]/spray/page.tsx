fb83748669cf661a27bb758404bc575b49e39bba
Misha123a <147885592+Misha123a@users.noreply.github.com>
2025-03-22 06:37:23 +0100

    feat: добавлен sticky для панели оружия

--- src/app/[locale]/spray/page.tsx
+++ src/app/[locale]/spray/page.tsx
@@ -17,165 +17,166 @@ type Weapon = {
   totalRecoilDiff: number;
 };
 
-const weapons: Weapon[] = [{
-  id: "ak47",
-  name: "AK47",
-  image: "https://i.ibb.co/Cxctp06/ak47-light.png",
-  cs2Spray: "https://files.catbox.moe/v3cdv6.webm",
-  csgoSpray: "https://files.catbox.moe/pk9gl8.webm",
-  recoilHeight: 11.06,
-  recoilWidth: 7.14,
-  totalRecoil: 78.93,
-  recoilHeightDiff: -0.69,
+const weapons: Weapon[] = [
+  {
+    id: "ak47",
+    name: "AK47",
+    image: "https://i.ibb.co/Cxctp06/ak47-light.png",
+    cs2Spray: "https://files.catbox.moe/v3cdv6.webm",
+    csgoSpray: "https://files.catbox.moe/pk9gl8.webm",
+    recoilHeight: 11.06,
+    recoilWidth: 7.14,
+    totalRecoil: 78.93,
+    recoilHeightDiff: -0.69,
+    recoilWidthDiff: -0.24,
+    totalRecoilDiff: -7.74
+  },
+  {
+    id: "m4a4",
+    name: "M4A4",
+    image: "https://i.ibb.co/G34G8yZP/m4a1-light.png",
+    cs2Spray: "https://files.catbox.moe/5qmcqq.webm",
+    csgoSpray: "https://files.catbox.moe/8vf73z.webm",
+    recoilHeight: 9.06,
+    recoilWidth: 6.98,
+    totalRecoil: 70.12,
+    recoilHeightDiff: -0.38,
+    recoilWidthDiff: -0.22,
+    totalRecoilDiff: -6.50
+  },
+  {
+    id: "m4a4-s",
+    name: "M4A1-S",
+    image: "https://i.ibb.co/6R521KF9/m4a1-silencer-light.png",
+    cs2Spray: "https://files.catbox.moe/9j2ztv.webm",
+    csgoSpray: "https://files.catbox.moe/juqkjw.webm",
+    recoilHeight: 5.93,
+    recoilWidth: 4.62,
+    totalRecoil: 27.42,
+    recoilHeightDiff: -0.38,
+    recoilWidthDiff: -0.13,
+    totalRecoilDiff: -2.57
+  },
+  {
+    id: "sg556",
+    name: "SG556",
+    image: "https://i.ibb.co/6JVxnFTk/sg556-light.png",
+    cs2Spray: "https://files.catbox.moe/phrpf0.webm",
+    csgoSpray: "https://files.catbox.moe/meo9a2.webm",
+    recoilHeight: 8.59,
+    recoilWidth: 9.7,
+    totalRecoil: 83.34,
+    recoilHeightDiff: -0.6,
+    recoilWidthDiff: -0.42,
+    totalRecoilDiff: -9.7
+  },
+  {
+      id: "aug",
+    name: "Aug",
+    image: "https://i.ibb.co/Ld6PZ5ND/aug-light.png",
+    cs2Spray: "https://files.catbox.moe/mgozrk.webm",
+    csgoSpray: "https://files.catbox.moe/kagjpa.webm",
+    recoilHeight: 7.84,
+    recoilWidth: 5.67,
+    totalRecoil: 44.48,
+    recoilHeightDiff: -0.41,
+    recoilWidthDiff: -0.26,
+    totalRecoilDiff: -4.5
+  },
+  {
+	id: "galil",
+  name: "Galil",
+  image: "https://i.ibb.co/VWwSqxSN/galilar-light.png",
+  cs2Spray: "https://files.catbox.moe/4doho7.webm",
+  csgoSpray: "https://files.catbox.moe/jt1gm2.webm",
+  recoilHeight: 7.31,
+  recoilWidth: 7.06,
+  totalRecoil: 51.61,
+  recoilHeightDiff: -0.5,
+  recoilWidthDiff: -0.32,
+  totalRecoilDiff: -6.01
+},
+{
+	id: "famas",
+  name: "Famas",
+  image: "https://i.ibb.co/LXCmvfRP/famas-light.png",
+  cs2Spray: "https://files.catbox.moe/vt0w7k.webm",
+  csgoSpray: "https://files.catbox.moe/aalw8g.webm",
+  recoilHeight: 6.96,
+  recoilWidth: 4.89,
+  totalRecoil: 32.04,
+  recoilHeightDiff: -0.47,
+  recoilWidthDiff: -0.17,
+  totalRecoilDiff: -3.61
+},
+{
+	id: "ump",
+  name: "Ump",
+  image: "https://i.ibb.co/gbTs7gKK/ump45-light.png",
+  cs2Spray: "https://files.catbox.moe/8iyhmz.webm",
+  csgoSpray: "https://files.catbox.moe/rjnerv.webm",
+  recoilHeight: 8.95,
+  recoilWidth: 3.56,
+  totalRecoil: 31.86,
+  recoilHeightDiff: -0.68,
+  recoilWidthDiff: -0.07,
+  totalRecoilDiff: -3.04
+},
+{
+	id: "mp7",
+  name: "MP7",
+  image: "https://i.ibb.co/b4q60DD/mp7-light.png",
+  cs2Spray: "https://files.catbox.moe/m6hkss.webm",
+  csgoSpray: "https://files.catbox.moe/fvhi4z.webm",
+  recoilHeight: 6.27,
+  recoilWidth: 3.39,
+  totalRecoil: 21.23,
+  recoilHeightDiff: -0.3,
+  recoilWidthDiff: -0.17,
+  totalRecoilDiff: -2.14
+},
+{
+	id: "mp9",
+  name: "MP9",
+  image: "https://i.ibb.co/vvQXV75V/mp9-light.png",
+  cs2Spray: "https://files.catbox.moe/cmxv01.webm",
+  csgoSpray: "https://files.catbox.moe/uunir8.webm",
+  recoilHeight: 9.13,
+  recoilWidth: 7.2,
+  totalRecoil: 65.75,
+  recoilHeightDiff: -0.68,
   recoilWidthDiff: -0.24,
-  totalRecoilDiff: -7.74
+  totalRecoilDiff: -7.23
 },
 {
-  id: "m4a4",
-  name: "M4A4",
-  image: "https://i.ibb.co/G34G8yZP/m4a1-light.png",
-  cs2Spray: "https://files.catbox.moe/5qmcqq.webm",
-  csgoSpray: "https://files.catbox.moe/8vf73z.webm",
-  recoilHeight: 9.06,
-  recoilWidth: 6.98,
-  totalRecoil: 70.12,
-  recoilHeightDiff: -0.38,
-  recoilWidthDiff: -0.22,
-  totalRecoilDiff: -6.50
-},
-{
-  id: "m4a4-s",
-  name: "M4A1-S",
-  image: "https://i.ibb.co/6R521KF9/m4a1-silencer-light.png",
-  cs2Spray: "https://files.catbox.moe/9j2ztv.webm",
-  csgoSpray: "https://files.catbox.moe/juqkjw.webm",
-  recoilHeight: 5.93,
-  recoilWidth: 4.62,
-  totalRecoil: 27.42,
-  recoilHeightDiff: -0.38,
-  recoilWidthDiff: -0.13,
-  totalRecoilDiff: -2.57
-},
-{
-  id: "sg556",
-  name: "SG556",
-  image: "https://i.ibb.co/6JVxnFTk/sg556-light.png",
-  cs2Spray: "https://files.catbox.moe/phrpf0.webm",
-  csgoSpray: "https://files.catbox.moe/meo9a2.webm",
-  recoilHeight: 8.59,
-  recoilWidth: 9.7,
-  totalRecoil: 83.34,
-  recoilHeightDiff: -0.6,
-  recoilWidthDiff: -0.42,
-  totalRecoilDiff: -9.7
-},
-{
-    id: "aug",
-  name: "Aug",
-  image: "https://i.ibb.co/Ld6PZ5ND/aug-light.png",
-  cs2Spray: "https://files.catbox.moe/mgozrk.webm",
-  csgoSpray: "https://files.catbox.moe/kagjpa.webm",
-  recoilHeight: 7.84,
-  recoilWidth: 5.67,
-  totalRecoil: 44.48,
+	id: "p90",
+  name: "P90",
+  image: "https://i.ibb.co/FSWhGQ9/p90-light.png",
+  cs2Spray: "https://files.catbox.moe/crqh9c.webm",
+  csgoSpray: "https://files.catbox.moe/a2bfw3.webm",
+  recoilHeight: 7.47,
+  recoilWidth: 6.15,
+  totalRecoil: 45.96,
   recoilHeightDiff: -0.41,
-  recoilWidthDiff: -0.26,
-  totalRecoilDiff: -4.5
+  recoilWidthDiff: -0.16,
+  totalRecoilDiff: -3.79
 },
 {
-id: "galil",
-name: "Galil",
-image: "https://i.ibb.co/VWwSqxSN/galilar-light.png",
-cs2Spray: "https://files.catbox.moe/4doho7.webm",
-csgoSpray: "https://files.catbox.moe/jt1gm2.webm",
-recoilHeight: 7.31,
-recoilWidth: 7.06,
-totalRecoil: 51.61,
-recoilHeightDiff: -0.5,
-recoilWidthDiff: -0.32,
-totalRecoilDiff: -6.01
-},
-{
-id: "famas",
-name: "Famas",
-image: "https://i.ibb.co/LXCmvfRP/famas-light.png",
-cs2Spray: "https://files.catbox.moe/vt0w7k.webm",
-csgoSpray: "https://files.catbox.moe/aalw8g.webm",
-recoilHeight: 6.96,
-recoilWidth: 4.89,
-totalRecoil: 32.04,
-recoilHeightDiff: -0.47,
-recoilWidthDiff: -0.17,
-totalRecoilDiff: -3.61
-},
-{
-id: "ump",
-name: "Ump",
-image: "https://i.ibb.co/gbTs7gKK/ump45-light.png",
-cs2Spray: "https://files.catbox.moe/8iyhmz.webm",
-csgoSpray: "https://files.catbox.moe/rjnerv.webm",
-recoilHeight: 8.95,
-recoilWidth: 3.56,
-totalRecoil: 31.86,
-recoilHeightDiff: -0.68,
-recoilWidthDiff: -0.07,
-totalRecoilDiff: -3.04
-},
-{
-id: "mp7",
-name: "MP7",
-image: "https://i.ibb.co/b4q60DD/mp7-light.png",
-cs2Spray: "https://files.catbox.moe/m6hkss.webm",
-csgoSpray: "https://files.catbox.moe/fvhi4z.webm",
-recoilHeight: 6.27,
-recoilWidth: 3.39,
-totalRecoil: 21.23,
-recoilHeightDiff: -0.3,
-recoilWidthDiff: -0.17,
-totalRecoilDiff: -2.14
-},
-{
-id: "mp9",
-name: "MP9",
-image: "https://i.ibb.co/vvQXV75V/mp9-light.png",
-cs2Spray: "https://files.catbox.moe/cmxv01.webm",
-csgoSpray: "https://files.catbox.moe/uunir8.webm",
-recoilHeight: 9.13,
-recoilWidth: 7.2,
-totalRecoil: 65.75,
-recoilHeightDiff: -0.68,
-recoilWidthDiff: -0.24,
-totalRecoilDiff: -7.23
-},
-{
-id: "p90",
-name: "P90",
-image: "https://i.ibb.co/FSWhGQ9/p90-light.png",
-cs2Spray: "https://files.catbox.moe/crqh9c.webm",
-csgoSpray: "https://files.catbox.moe/a2bfw3.webm",
-recoilHeight: 7.47,
-recoilWidth: 6.15,
-totalRecoil: 45.96,
-recoilHeightDiff: -0.41,
-recoilWidthDiff: -0.16,
-totalRecoilDiff: -3.79
-},
-{
-id: "mp5",
-name: "MP5",
-image: "https://i.ibb.co/Hf0dc03r/mp5-light.png",
-cs2Spray: "https://files.catbox.moe/ft8pq3.webm",
-csgoSpray: "https://files.catbox.moe/klloqq.webm",
-recoilHeight: 6.27,
-recoilWidth: 3.39,
-totalRecoil: 21.23,
-recoilHeightDiff: -0.3,
-recoilWidthDiff: -0.17,
-totalRecoilDiff: -2.14
-}];
+	id: "mp5",
+  name: "MP5",
+  image: "https://i.ibb.co/Hf0dc03r/mp5-light.png",
+  cs2Spray: "https://files.catbox.moe/ft8pq3.webm",
+  csgoSpray: "https://files.catbox.moe/klloqq.webm",
+  recoilHeight: 6.27,
+  recoilWidth: 3.39,
+  totalRecoil: 21.23,
+  recoilHeightDiff: -0.3,
+  recoilWidthDiff: -0.17,
+  totalRecoilDiff: -2.14
+}
+];
 
 export default function SprayPatternsPage() {
-
   const [playing, setPlaying] = useState<string | null>(null);
   const refs = useRef<Record<string, HTMLDivElement>>({});
 
@@ -200,7 +201,8 @@ export default function SprayPatternsPage() {
 
   return (
     <div className="max-w-7xl mx-auto py-10 px-4 text-white">
-      <div className="flex space-x-4 overflow-x-auto border-b border-blue-500 pb-2 mb-8">
+      {/* Sticky panel */}
+      <div className="sticky top-0 z-50 bg-[#0B0E19] px-4 py-2 border-b border-blue-500 flex space-x-4 overflow-x-auto">
         {weapons.map((weapon) => (
           <button
             key={weapon.id}
